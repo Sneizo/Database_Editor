a1277810e98b4f237079d81af3657c7fa35e2a4a
Sneizo <dulac.e1602102@etud.univ-ubs.fr>
2017-06-12 15:05:44 +0200

    Première modification

---
+++ Test2.java
@@ -0,0 +1,29 @@
+package vue;
+
+/**
+ * the class 2
+ */
+ 
+public class Test2 {
+	
+	/**
+	 * the constructor
+	 */
+	 
+	public Test2() {
+	}
+	
+	/**
+	 * start
+	 * @return true if ok
+	 */
+	 
+	public boolean debut() {
+		return true;
+	}
+	
+	public void start(){
+
+		
+	}
+}
