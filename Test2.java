8ca02d6cac06986f338e601540556267e7eaf4f5
MaelBRGD <maouldu98@gmail.com>
2017-06-12 15:53:14 +0200

    Même test2

---
+++ Test2.java
@@ -0,0 +1,25 @@
+//package controller;
+
+/**
+ * the class 2
+ */
+ 
+public class Test2 {
+	
+	/**
+	 * the constructor
+	 */
+	 
+	public Test2() {
+	}
+	
+	/**
+	 * start
+	 * @return true if ok
+	 */
+	 
+	public boolean debut() {
+		return true;
+	}
+	
+}
