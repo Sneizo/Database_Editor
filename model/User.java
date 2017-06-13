66ca32286dc9dbc00f1fc558696eb59059e33546
MaelBRGD <maouldu98@gmail.com>
2017-06-13 10:51:23 +0200

    Nouvelle classe User

---
+++ model/User.java
@@ -0,0 +1,22 @@
+package model;
+
+public class User {
+	
+	private String login;
+	private String passwd;
+	
+	public User(String login, String passwd) {
+		if(login != null && passws != null) {
+			this.login = login;
+			this.passwd = passwd;
+		}
+	}
+	
+	public String getLogin() {
+		return this.login;
+	}
+	
+	public String getPasswd() {
+		return this.passwd;
+	}
+}
