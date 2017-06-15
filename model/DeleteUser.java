c1966d6c2a47728595fa8286c196d412a2bc997e
Sneizo <dulac.e1602102@etud.univ-ubs.fr>
2017-06-15 09:46:34 +0200

    Saisie du passAdmin.

--- model/DeleteUser.java
+++ model/DeleteUser.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class DeleteUser {
 	
@@ -21,7 +22,11 @@ public class DeleteUser {
 
 	private void connexion() {
 		try {
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:Database","SYSTEM","24u4kvkH-p05m9");
+			Scanner sc = new Scanner(System.in);
+			System.out.println("[user] Admin password: ");
+			System.out.print("[user] ");
+			String passAdmin = sc.nextLine();
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:Database","SYSTEM",passAdmin);
 			stmt = conn.createStatement();
 		} catch (SQLException e) {
 			System.out.println("[user] User login or password incorrect.");
